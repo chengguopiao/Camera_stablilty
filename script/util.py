c88138248c0baa3a1d940825e74fd9173531358a
chengguopiao <chengguo.piao@borqs.com>
2014-05-23 16:32:24 +0800

    Update util.py

--- script/util.py
+++ script/util.py
@@ -271,6 +271,7 @@ class TouchButton():
         # capture single image
         def _singlecapture():
             d(resourceId = CPTUREBUTTON_RESOURCEID).click.wait()
+            time.sleep(2)
         # capture smile image
         def _smilecapture():
             d(resourceId = CPTUREBUTTON_RESOURCEID).click.wait()
